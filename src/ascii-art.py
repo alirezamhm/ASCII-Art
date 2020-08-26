9a2649de461d2266901e15300f79b568c2187854
alirezamhm <alireza.am1379@gmail.com>
2020-08-26 18:06:57 +0430

    debugg list out of range

--- src/ascii-art.py
+++ src/ascii-art.py
@@ -13,14 +13,14 @@ data = list(im.getdata())
 pixels = [data[i:i+width] for i in range(0, len(data), width)]
 for row in range(len(pixels)):
     for col in range(len(pixels[row])):
-        pixels[row][col] = sum(pixels[row][col]) // 3
+        pixels[row][col] = sum(pixels[row][col])// 3
 
 characters = list("`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$") # ascii characters sorted by brightness
 ratio = 255/len(characters)
 for row in range(len(pixels)):
     for col in range(len(pixels[row])):
         brightness = pixels[row][col]
-        pixels[row][col] = characters[int(brightness//ratio)]
+        pixels[row][col] = characters[int(brightness//ratio)-1]
 
 def print_image(pixels: list):
     for row in range(len(pixels)):
